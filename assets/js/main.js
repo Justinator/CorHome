68bfbbfb3a3c9d2245685954b514cb41a1966533
Justinator <32310311+Justinator@users.noreply.github.com>
2020-08-31 11:24:10 -0500

    built out the homepage of the site, started testing setup for barba and gsap for page transitions and other content animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,55 +11,73 @@ function toggleMobileMenu() {
     hamburger.classList.toggle("is-active");
 }
 
-// var parallaxTitle1 = document.querySelector(".parallax-1");
-// var parallaxTitle2 = document.querySelector(".parallax-2");
-// var parallaxTitle3 = document.querySelector(".parallax-3");
+(function () {
+    var current = location.pathname.split('/')[1];
+    if (current === "") return;
+    var menuItems = document.querySelectorAll('.nav-list-item a');
+    for (var i = 0, len = menuItems.length; i < len; i++) {
+        if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+            menuItems[i].className = "current";
+        }
+    }
+})();
 
-// window.addEventListener("scroll", function () {
-//     var scrollPosition = window.scrollY;
-
-//     parallaxTitle1.style.transform = "translateX(-" + scrollPosition / 50 + "%)";
-//     parallaxTitle2.style.transform = "translateX(-" + scrollPosition / 50 + "%)";
-//     parallaxTitle3.style.transform = "translateX(-" + scrollPosition / 50 + "%)";
-// })
-// gsap scroll trigger
-function scrollTrigger() {
-    const blueback = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".blue-background",
-            start: "top bottom",
-            end: "bottom top",
-        }
+function scrollTop() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
     });
-    blueback.to(".blue-background", { width: "70%" });
-    const parallax1 = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".parallax-1",
-            start: "top bottom",
-            end: "bottom top",
-            scrub: true,
-        }
-    });
-    parallax1.to(".parallax-1", { translateX: "-40%" });
-    const parallax2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".parallax-2",
-            start: "top bottom",
-            end: "bottom top",
-            scrub: true,
-        }
-    });
-    parallax2.to(".parallax-2", { translateX: "-20%" });
-    const parallax3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".parallax-3",
-            start: "top bottom",
-            end: "bottom top",
-            scrub: true,
-        }
-    });
-    parallax3.to(".parallax-3", { translateX: "-20%" });
 }
+
+// gsap scroll trigger
+
+const zoom1 = gsap.to(".zoom-1", {
+    scale: 1.1,
+});
+ScrollTrigger.create({
+    trigger: ".zoom-1",
+    animation: zoom1,
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+});
+const zoom2 = gsap.to(".zoom-2", {
+    scale: 1.1,
+});
+ScrollTrigger.create({
+    trigger: ".zoom-2",
+    animation: zoom2,
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+});
+const zoom3 = gsap.to(".zoom-3", {
+    scale: 1.1,
+});
+ScrollTrigger.create({
+    trigger: ".zoom-3",
+    animation: zoom3,
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+});
+// TO-DO continue to work on writing this into a forEach of sorts to grab all zoom images rather than writing this 3+ times
+var zoomImages = gsap.utils.toArray(".zoom");
+console.log(zoomImages);
+// gsap.utils.toArray(".zoom").forEach(zoom => {
+//     var tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: ".zoom",
+//             start: "top bottom",
+//             end: "bottom top",
+//             scrub: true,
+//         }
+//     });
+//     tl.to(".zoom", {
+//         scale: 1.1,
+//     });
+// });
 
 function swiperInit() {
     // init swiper slider for portfolio
@@ -85,15 +103,6 @@ function swiperInit() {
     });
 }
 
-// scroll progress bar indicator
-var progress = document.querySelector(".progress-container");
-var progressBar = document.querySelector(".progress-bar");
-var totalHeight = document.body.scrollHeight - window.innerHeight;
-
-window.onscroll = function () {
-    var progress = (window.pageYOffset / totalHeight) * 100;
-    progressBar.style.height = progress + "%";
-}
 // loading screen style animation
 function delay(n) {
     n = n || 2000;
@@ -104,26 +113,31 @@ function delay(n) {
     });
 }
 
-function pageTransition() {
+function pageTransitionOut() {
     var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: .5,
-        width: "100%",
-        left: "0%",
+    tl.to(".site-main", {
+        duration: 1,
+        opacity: 0,
         ease: "Expo.easeInOut",
     });
-    tl.to(".loading-screen", {
-        duration: .5,
-        width: "100%",
-        left: "100%",
+    tl.to("window.scroll", {
+        top: 0,
+        left: 0,
         ease: "Expo.easeInOut",
-        delay: 0.3,
     });
-    tl.set(".loading-screen", { left: "-100%" });
     if (mobileNav.classList.contains("nav-open")) {
         mobileNav.classList.remove("nav-open");
         hamburger.classList.remove("is-active");
     }
+}
+
+function pageTransitionIn() {
+    var tl = gsap.timeline();
+    tl.from(".site-main", {
+        duration: 1,
+        opacity: 0,
+        ease: "Expo.easeInOut",
+    });
 }
 
 function contentAnimation() {
@@ -162,27 +176,48 @@ function contentAnimation() {
     });
 }
 
+// scroll to the top of the page
+barba.hooks.enter(() => {
 
+    window.scrollTo(0, 0);
+
+});
 
 barba.init({
     sync: true,
 
     transitions: [
         {
+            before: ({ current, next, trigger }) => {
+
+                // get the menu element
+                let menu = document.querySelector('.nav-item');
+
+                // select the menu item depending on the next URL (you can do that in many ways)
+                let nextItem = menu.querySelector(`a[href="${next.url.href}"]`);
+
+                // reset the active menu item and set the next item as "active" (if there is one)
+                if (nextItem !== null) {
+                    menu.querySelector('.current').classList.remove('current');
+                    nextItem.classList.add('current');
+                }
+            },
             async leave(data) {
                 const done = this.async();
 
-                pageTransition();
+                pageTransitionOut();
                 await delay(1000);
                 done();
             },
             async enter(data) {
                 contentAnimation();
+                pageTransitionIn();
                 scrollTrigger();
                 swiperInit();
             },
             async once(data) {
                 contentAnimation();
+                pageTransitionIn();
                 scrollTrigger();
                 swiperInit();
             },
