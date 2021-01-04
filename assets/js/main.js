5a5a2b4e0fc041e4914a00c2f0f9203ec6f74c03
Justinator <32310311+Justinator@users.noreply.github.com>
2021-01-03 20:19:44 -0600

    testing on last method of fixing the locomotive scroll issue on the gallery page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ import barba from '@barba/core';
 import barbaPrefetch from '@barba/prefetch';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import LocomotiveScroll from 'locomotive-scroll';
 import { pageTransitionOut, pageTransitionIn, contentAnimation, updateMenu } from './partials';
 
 barba.use(barbaPrefetch);
@@ -18,6 +19,7 @@ function toggleMobileMenu() {
     hamburger.classList.toggle("is-active");
 }
 
+let scroll;
 const selectAll = (e) => document.querySelectorAll(e);
 
 function fadeInContent() {
@@ -35,6 +37,7 @@ function fadeInContent() {
             trigger: introSection,
             start: "top bottom-=50",
             toggleActions: "play none none reset",
+            scroller: ".smooth-scroll",
         }
     });
     gsap.utils.toArray(fadeUp).forEach((fade) => {
@@ -46,6 +49,7 @@ function fadeInContent() {
             scrollTrigger: {
                 trigger: fade,
                 toggleActions: "play none none reset",
+                scroller: ".smooth-scroll",
             }
         })
     });
@@ -59,6 +63,7 @@ function fadeInContent() {
             trigger: fadeWrapper,
             start: "top bottom-=25",
             toggleActions: "play none none reset",
+            scroller: ".smooth-scroll",
         }
     });
     if (document.body.contains(blog)) {
@@ -72,6 +77,7 @@ function fadeInContent() {
                 trigger: blog,
                 start: "top bottom-=25",
                 toggleActions: "play none none reset",
+                scroller: ".smooth-scroll",
             }
         });
     }
@@ -86,6 +92,7 @@ function initZoom() {
             scrollTrigger: {
                 trigger: section,
                 scrub: true,
+                scroller: ".smooth-scroll",
             }
         })
     });
@@ -96,6 +103,17 @@ function homepageAnimations() {
     initZoom();
 }
 
+function removeScrollbar() {
+    //Remove Old Locomotive Scrollbar otherwise there will be two
+    const scrollbar = selectAll('.c-scrollbar');
+
+    console.log(scrollbar);
+
+    if (scrollbar.length > 1) {
+        scrollbar[0].remove();
+    }
+}
+
 function initPageTransitions() {
     // do something before the transition starts
     barba.hooks.before(() => {
@@ -104,7 +122,10 @@ function initPageTransitions() {
 
     // do something after the transition finishes
     barba.hooks.after(() => {
+        // reinit locomotive scroll
+        scroll.init();
         homepageAnimations();
+        removeScrollbar();
     });
 
     // scroll to the top of the page
@@ -119,6 +140,7 @@ function initPageTransitions() {
             once(data) {
                 contentAnimation();
                 // do something once on the initial page load
+                initSmoothScroll(data.next.container);
                 homepageAnimations();
             },
             async leave(data) {
@@ -133,10 +155,49 @@ function initPageTransitions() {
             async beforeEnter(data) {
                 contentAnimation();
                 ScrollTrigger.getAll().forEach(t => t.kill());
+                scroll.destroy();
+                initSmoothScroll(data.next.container);
             }
 
         }]
     });
+
+    function initSmoothScroll(container) {
+        // borrowed from the pens https://codepen.io/mulegoat/project/editor/XvJVNP and https://codepen.io/GreenSock/pen/1dc38ca14811bc76e25c4b8c686b653d
+        scroll = new LocomotiveScroll({
+            el: container.querySelector('[data-scroll-container]'),
+            smooth: true,
+            getDirection: true
+        });
+
+        scroll.on("scroll", ScrollTrigger.update);
+
+        ScrollTrigger.scrollerProxy('[data-scroll-container]', {
+            scrollTop(value) {
+                return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+            }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+            getBoundingClientRect() {
+                return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+            },
+            // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+            pinType: container.querySelector('[data-scroll-container]').style.transform ? "transform" : "fixed"
+        });
+
+        // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+        ScrollTrigger.addEventListener('refresh', () => scroll.update());
+
+        // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+        ScrollTrigger.refresh();
+    }
 }
+
+// function delay(n) {
+//     n = n || 2000;
+//     return new Promise((done) => {
+//         setTimeout(() => {
+//             done();
+//         }, n);
+//     });
+// }
 
 initPageTransitions();
